879cf332004838e75b7a8b4f83ac26c3a1e3500c
fulgay <firat.ulg@gmail.com>
2020-07-22 09:39:46 +0300

    hello

---
+++ src/main/java/com/team2/App.java
@@ -0,0 +1,13 @@
+package com.team2;
+
+/**
+ * App
+ *
+ * @author Fırat ÜLGAY
+ * @since 5.230.0
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
