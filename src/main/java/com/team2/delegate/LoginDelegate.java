7ceb95387f8b9f22e5897467862d54c82ffcfe55
Muhammed Enes YAKUT <enesyakut29@gmail.com>
2020-07-22 14:55:59 +0300

    test

--- src/main/java/com/team2/delegate/LoginDelegate.java
+++ src/main/java/com/team2/delegate/LoginDelegate.java
@@ -1,4 +1,25 @@
 package com.team2.delegate;
 
-public class LoginDelegate {
+import com.team2.service.UserService;
+
+import java.sql.SQLException;
+
+public class LoginDelegate
+{
+    private UserService userService;
+
+    public UserService getUserService()
+    {
+        return this.userService;
+    }
+
+    public void setUserService(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    public boolean isValidUser(String name, String password) throws SQLException
+    {
+        return userService.isValidUser(name, password);
+    }
 }
